374cd807b9712b0f9563a6ab0b5b92dc498638a3
_sursill <32005471+sursill@users.noreply.github.com>
2022-10-17 08:29:28 +0800

    Add ufw enable

--- setup.sh
+++ setup.sh
@@ -9,9 +9,15 @@ sudo apt install -y git \
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || \
-                    
+
+# Sync snap local db
+snap refresh
+
 # Install btop for monitoring system resources
 snap install btop
 
 # Install docker
 snap install docker
+
+# Enable and config ufw
+yes | ufw enable
